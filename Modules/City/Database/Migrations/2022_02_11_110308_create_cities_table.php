598c0a8a6f3a58ca9165d4ba48be47cbc144d495
Rezakarimi376 <kreza672102@gmail.com>
2022-02-11 14:34:14 +0330

    add City module

---
+++ Modules/City/Database/Migrations/2022_02_11_110308_create_cities_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->unsignedBigInteger('province_id');
+            $table->timestamps();
+            $table->foreign('province_id')->references('id')->on('provinces');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}
