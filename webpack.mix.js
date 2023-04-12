1293f3e0a3e9c33850939e71868662a11486ff7d
rezak9876 <rezak9876.official@gmail.com>
2023-04-12 22:32:20 +0430

    fix: responsive of leaflet map in panel change source file order of laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,19 +10,12 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-// mix.js('resources/vueadmin/app.js', 'public/js')
-//  .postCss('resources/css/app.css', 'public/css', [
-//      //
-//  ]);
+mix.js('node_modules/leaflet/dist/leaflet.js', 'public/packages/leaflet')
+    .css('node_modules/leaflet/dist/leaflet.css', 'public/packages/leaflet')
 
 mix.js('resources/vueadmin/app.js', 'public/js/admin')
     .sourceMaps()
     .vue();
-
-mix.js('node_modules/leaflet/dist/leaflet.js', 'public/packages/leaflet')
-    .postCss('node_modules/leaflet/dist/leaflet.css', 'public/packages/leaflet')
-
 
 if (mix.inProduction()) {
     mix.version();
